bc958c4382eba941b537db82c645c52a78e56ab4
unknown <christopher.roelofs@gmail.com>
2022-02-14 11:55:54 -0500

    add extra scripts to extras release archive

--- build_extra.py
+++ build_extra.py
@@ -12,4 +12,8 @@ os.system('pyinstaller -i icon.ico -n DatabaseManager --clean -y -F --windowed d
 os.system('pyinstaller -i icon.ico -n DatabaseTool --clean -y -F  database_tool.py')
 shutil.copyfile('Create Patch.bat', '{0}/Create Patch.bat'.format("dist"))
 shutil.copyfile('Import Patch.bat', '{0}/Import Patch.bat'.format("dist"))
+shutil.copyfile('splitcores.json', '{0}/splitcores.json'.format("dist"))
+shutil.copyfile('splitcores.py', '{0}/splitcores.py'.format("dist"))
+shutil.copyfile('splitcores.sh', '{0}/splitcores.sh'.format("dist"))
+shutil.copyfile('update_and_copy.sh', '{0}/update_and_copy.sh'.format("dist"))
 shutil.make_archive(os.path.join("release","extra"), 'zip', "dist")
