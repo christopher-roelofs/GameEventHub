51e139f916c577286805fc60f8be89076c44cfa8
unknown <christopher.roelofs@gmail.com>
2022-02-16 10:10:06 -0500

    remove old manage_event

--- event_manager.py
+++ event_manager.py
@@ -11,19 +11,6 @@ if os.path.exists("pubsub.json"):
         with open("pubsub.json") as subsub_json:
             pubsub = json.load(subsub_json)
 
-def manage_event2(event):
-    logger.event(f"Managing {event.event} from {event.publisher}")
-    for subscriber in subscribers:
-        if subscriber in pubsub["subscribers"]:
-            if pubsub["subscribers"][subscriber]["status"] == "enabled":
-                subscriptions = pubsub["subscribers"][subscriber]["subscribed_events"]
-                for subscription in subscriptions:
-                    for key in subscription.keys():
-                        if key == event.event:
-                            for action in subscription[key]:
-                                if action["status"] == "enabled":
-                                    subscribers[subscriber]["handle_event"]["function"](event,action)
-
 def manage_event(event):
     logger.event(f"Managing {event.event} from {event.publisher}")
     for subscriber in pubsub["subscribers"]:
